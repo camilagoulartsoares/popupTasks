015347f6ccf02093604637b1af2e580c702c4da6
Camila Soares <camilagoulartsoares@yahoo.com>
2025-12-17 17:24:07 -0300

    changes

--- src/contentScript.jsx
+++ src/contentScript.jsx
@@ -4,40 +4,20 @@ import App from "./App";
 
 const CONTAINER_ID = "popup-tasks-widget";
 
-function injectApp() {
-  if (document.getElementById(CONTAINER_ID)) return;
+const container = document.createElement("div");
+container.id = CONTAINER_ID;
 
-  const container = document.createElement("div");
-  container.id = CONTAINER_ID;
-  document.body.appendChild(container);
+Object.assign(container.style, {
+  position: "fixed",
+  bottom: "12px",
+  right: "12px",
+  zIndex: "2147483647"
+});
 
-  Object.assign(container.style, {
-    position: "fixed",
-    bottom: "12px",
-    right: "12px",
-    width: "320px",
-    zIndex: "2147483647",
-    background: "transparent",
-    margin: "0",
-    padding: "0",
-    boxShadow: "none",
-    borderRadius: "0",
-    overflow: "visible"
-  });
+document.body.appendChild(container);
 
-
-  const root = ReactDOM.createRoot(container);
-
-  const handleClose = () => {
-    root.unmount();
-    container.remove();
-  };
-
-  root.render(
-    <React.StrictMode>
-      <App onClose={handleClose} />
-    </React.StrictMode>
-  );
-}
-
-injectApp();
+const root = ReactDOM.createRoot(container);
+root.render(<App onClose={() => {
+  root.unmount();
+  container.remove();
+}} />);
